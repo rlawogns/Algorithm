411b89af4737fcfa3bbd94cdc42d9f59d7d5d572
Jaehun.Kim <rlawogns0411@naver.com>
2021-07-28 22:33:13 +0900

    Create 1051_숫자 정사각형.py

---
+++ 1051_숫자 정사각형.py
@@ -0,0 +1,16 @@
+import sys
+n,m=map(int,input().split())
+p=min(n,m)
+l=[]
+for i in range(n)
+   l.append(list(map(int,sys.stdin.readline().strip())))
+c=0
+for i in range(n)
+   for j in range(m)
+      for k in range(1,p)
+         if not(in and jm and i+kn and j+km)
+            break
+         if l[i][j+k]==l[i+k][j]==l[i+k][j+k]==l[i][j]
+            c=max(c,k)
+c+=1
+print(cc)

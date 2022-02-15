7c07c50ee241026482685a7a77201d735d0145af
Jaehun.Kim <rlawogns0411@naver.com>
2022-02-16 00:31:40 +0900

    Create 1927_최소 힙.py

---
+++ 1927_최소 힙.py
@@ -0,0 +1,13 @@
+from sys import stdin
+import heapq
+n= int(input())
+hq=[]
+for _ in range(n):
+    m=int(stdin.readline()[:-1])
+    if m==0:
+        if len(hq)==0:
+            print(0)
+        else:
+            print(heapq.heappop(hq))
+    else:
+        heapq.heappush(hq,m)
